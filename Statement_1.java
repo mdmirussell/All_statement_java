c76ff83b912513077cae7c2ffbf678e89ba26e48
rcode4243 <95379583+mdmirussell@users.noreply.github.com>
2022-12-22 21:45:59 +0600

    Initial commit

---
+++ Statement_1.java
@@ -0,0 +1,10 @@
+public class Statement_1 {
+    public static void main(String [] args){
+        // use next line use \n
+        System.out.println( "Java \nfinaly \nRun ");
+        // print line brack 
+        System.out.println( "Java finaly Run ");
+       // just print string
+        System.out.print(  "Java finaly Run ");
+    }
+}
